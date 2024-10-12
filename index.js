a71f5c46db7826a25a52346c99466affc5e83c8a
Igor Mrtvac <ddp1329@gmail.com>
2024-10-12 15:50:21 +0000

    setup

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-console.log("commit")
 var app = express();
+const mongoose = require('mongoose')
+const multer = require("multer");
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+mongoose.connect(process.env.MONGO_URI)
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+const Schema = mongoose.Schema
+
+const storage = multer.memoryStorage()
+const upload = multer({storage})
+const fileSchema = new Schema({
+  name: String,
+  type: String,
+  size: Number
+})
+
+const File = mongoose.model("File", userSchema)
+
+app.post('/api/fileanalyse', async (req,res) => {
+
+})
 
 
 
